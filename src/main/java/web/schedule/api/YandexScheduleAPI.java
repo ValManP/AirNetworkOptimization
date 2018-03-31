610f2de940e139e21c826b45785461b4c40e1fd3
ValManP <valpozd@mail.ru>
2018-03-31 09:03:24 +0300

    Add YandexScheduleAPI

---
+++ src/main/java/web/schedule/api/YandexScheduleAPI.java
@@ -0,0 +1,42 @@
+package web.schedule.api;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class YandexScheduleAPI {
+    private static String urlString = "https://api.rasp.yandex.net/v3.0/schedule/?";
+
+    public void getSchedule() {
+        try {
+
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/json");
+
+            if (connection.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + connection.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (connection.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            connection.disconnect();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
